07db68c25df4607f631a88a2b32adf3304ca526d
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-02-24 23:29:56 +0200

    Add Solution for MaxArea Problem

--- NeetCodeProblems/Program.cs
+++ NeetCodeProblems/Program.cs
@@ -443,6 +443,27 @@ namespace NeetCodeProblems
             }
             return result;
         }
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = 0;
+
+            while (left < right)
+            {
+                int h = Math.Min(height[left], height[right]);
+                int width = right - left;
+                maxArea = Math.Max(maxArea, h * width);
+
+                // Move the pointer with the smaller height
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return maxArea;
+        }
     }
 
 }
