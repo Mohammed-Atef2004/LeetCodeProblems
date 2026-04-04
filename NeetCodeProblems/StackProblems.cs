1599098d6bf4dcd187016c29d3433196711a1d2c
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-04-04 21:12:30 +0200

    Add Solutions for Make Stack, Make Stack using Queues, Make Queues using Stack

--- NeetCodeProblems/StackProblems.cs
+++ NeetCodeProblems/StackProblems.cs
@@ -130,7 +130,97 @@ namespace NeetCodeProblems
                     }
                 }
             }
+
             return stack.Count == 0;
+        }
+    }
+    /// <summary>
+    /// Note: Implementation for the stack using two queues 
+    /// </summary>
+    public class MyStack
+    {
+        Queue<int> Main;
+        Queue<int> Temp;
+        public MyStack()
+        {
+            Main = new Queue<int>();
+            Temp = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            if (Main.Count == 0) Main.Enqueue(x);
+            else
+            {
+                Temp.Enqueue(x);
+                while (Main.Count > 0)
+                {
+                    Temp.Enqueue(Main.Dequeue());
+                }
+                Main.Clear();
+                while (Temp.Count > 0)
+                {
+                    Main.Enqueue(Temp.Dequeue());
+                }
+            }
+        }
+
+        public int Pop()
+        {
+            return Main.Dequeue();
+        }
+
+        public int Top()
+        {
+            int x = Main.Dequeue();
+            Push(x);
+            return x;
+        }
+
+        public bool Empty()
+        {
+            return Main.Count == 0;
+        }
+    }
+    public class MyQueue
+    {
+        Stack<int> Main;
+        Stack<int> Temp;
+
+        public MyQueue()
+        {
+            Main = new Stack<int>();
+            Temp = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            while (Main.Count > 0)
+            {
+                Temp.Push(Main.Pop());
+            }
+
+            Main.Push(x);
+
+            while (Temp.Count > 0)
+            {
+                Main.Push(Temp.Pop());
+            }
+        }
+
+        public int Pop()
+        {
+            return Main.Pop();
+        }
+
+        public int Peek()
+        {
+            return Main.Peek();
+        }
+
+        public bool Empty()
+        {
+            return Main.Count == 0;
         }
     }
 }
