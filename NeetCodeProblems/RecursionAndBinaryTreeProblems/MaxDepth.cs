cabc80de714a793cf34f14eb3b1b2da342799bfe
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-04-07 07:12:49 +0200

    Add solution for MaxDepth Problem

---
+++ NeetCodeProblems/RecursionAndBinaryTreeProblems/MaxDepth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCodeProblems.RecursionAndBinaryTreeProblems
+{
+    public partial class RecursionAndBinaryTreeProblems
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int leftDepth = MaxDepth(root.left);
+            int rightDepth = MaxDepth(root.right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+
+    }
+}
